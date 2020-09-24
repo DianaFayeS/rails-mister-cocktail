9b3d5294cf5b6361ce0fab3ccafcd8cde45210a1
DS <dianakondro@gmail.com>
2020-09-24 16:30:05 +0200

    third commit

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,19 +1,18 @@
 class CocktailsController < ApplicationController
-  
   def index
-  	@cocktails = Cocktail.all
+    @cocktails = Cocktail.all
   end
 
   def new
-  	@cocktail = Cocktail.new
+    @cocktail = Cocktail.new
   end
 
   def show
-  	@cocktail = Cocktail.find(params[:id])
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def create
-  	@cocktail = Cocktail.new(cocktail_params)
+    @cocktail = Cocktail.new(cocktail_params)
   	if @cocktail.save
       redirect_to :cocktails_path
     else render :new
@@ -30,4 +29,3 @@ end
     params.require(:cocktail).permit(:name, :photo)
   end
 end
-
